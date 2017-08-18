274548ae72e66a91370f9df56335c878e2b0b1ce
Luca Bolognese <lucabol@microsoft.com>
2017-08-18 12:02:35 +0200

    + Ubuntu dotnetcore install bash script

---
+++ Artifacts/DotNetCoreUbuntu1404/DotNetCoreUbuntu.sh
@@ -0,0 +1,5 @@
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install -q dotnet-sdk-2.0.0
